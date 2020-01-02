652b232c6b514105e9e9d18ad913dcb3bf2e4634
Jim <wowdacom@gmail.com>
2020-01-02 18:08:02 +0800

    update

--- src/components/BlackHawk.vue
+++ src/components/BlackHawk.vue
@@ -5,14 +5,28 @@
         'black-hawk__backgound': true,
         'black-hawk__backgound--active': contentEnterFlag[1],
       }"
-      style="backgroundImage: url('https://s.newtalk.tw/album/news/349/5e0d6d464349f.jpg')"
+      style="backgroundImage: url('https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_1.jpg')"
     />
     <div
       :class="{
         'black-hawk__backgound': true,
         'black-hawk__backgound--active': contentEnterFlag[2],
       }"
-      style="backgroundImage: url('https://s.newtalk.tw/album/news/349/5e0d56a697b60.jpg')"
+      style="backgroundImage: url('https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_2.jpg')"
+    />
+    <div
+      :class="{
+        'black-hawk__backgound': true,
+        'black-hawk__backgound--active': contentEnterFlag[3],
+      }"
+      style="backgroundImage: url('https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_3.jpg')"
+    />
+    <div
+      :class="{
+        'black-hawk__backgound': true,
+        'black-hawk__backgound--active': contentEnterFlag[4],
+      }"
+      style="backgroundImage: url('https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_4.jpg')"
     />
     <div ref="content1" class="black-hawk__content">
       <div class="black-hawk__content__text-wrapper">
@@ -23,6 +37,22 @@
       </div>
     </div>
     <div ref="content2" class="black-hawk__content">
+      <div class="black-hawk__content__text-wrapper">
+        <p>閨 中 少 婦 不 知 愁 </p>
+        <p>春 日 凝 妝 上 翠 樓</p>
+        <p>忽 見 陌 頭 楊 柳 色</p>
+        <p>悔 教 夫 婿 覓 封 侯</p>
+      </div>
+    </div>
+    <div ref="content3" class="black-hawk__content">
+      <div class="black-hawk__content__text-wrapper">
+        <p>閨 中 少 婦 不 知 愁 </p>
+        <p>春 日 凝 妝 上 翠 樓</p>
+        <p>忽 見 陌 頭 楊 柳 色</p>
+        <p>悔 教 夫 婿 覓 封 侯</p>
+      </div>
+    </div>
+    <div ref="content4" class="black-hawk__content">
       <div class="black-hawk__content__text-wrapper">
         <p>閨 中 少 婦 不 知 愁 </p>
         <p>春 日 凝 妝 上 翠 樓</p>
@@ -41,6 +71,8 @@ export default {
       contentEnterFlag: {
         1: false,
         2: false,
+        3: false,
+        4: false
       }
     };
   },
@@ -48,17 +80,31 @@ export default {
     handleScroll() {
       const content_1 = this.$refs.content1.getBoundingClientRect();
       const content_2 = this.$refs.content2.getBoundingClientRect();
+      const content_3 = this.$refs.content3.getBoundingClientRect();
+      const content_4 = this.$refs.content4.getBoundingClientRect();
 
-      if (content_1.top < window.innerHeight && content_1.bottom > 0) {
+      if (content_1.top < 0 && content_1.bottom > 0) {
         this.contentEnterFlag[1] = true;
       } else {
         this.contentEnterFlag[1] = false;
       }
 
-      if (content_2.top < window.innerHeight && content_2.bottom > 0) {
+      if (content_2.top < 0 && content_2.bottom > 0) {
         this.contentEnterFlag[2] = true;
       } else {
         this.contentEnterFlag[2] = false;
+      }
+
+      if (content_3.top < 0 && content_3.bottom > 0) {
+        this.contentEnterFlag[3] = true;
+      } else {
+        this.contentEnterFlag[3] = false;
+      }
+
+      if (content_4.top < 0 && content_4.bottom > 0) {
+        this.contentEnterFlag[4] = true;
+      } else {
+        this.contentEnterFlag[4] = false;
       }
     },
   },
@@ -72,6 +118,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.black-hawk {
+  background-color: #dedede;
+}
 .black-hawk__backgound {
   pointer-events: none;
   position: fixed;
@@ -81,12 +130,13 @@ export default {
   height: 100vh;
   background-size: cover;
   opacity: 0;
-  transition: .333s ease-in-out;
+  transition: .5s ease-in-out;
 }
 .black-hawk__backgound--active {
   opacity: 1;
 }
 .black-hawk__content {
+  border: solid 1px red;
   position: relative;
   width: 100%;
   height: 100vh;
@@ -94,9 +144,13 @@ export default {
   justify-content: center;
   align-items: center;
 }
+/* .black-hawk__content:first-child {
+  align-items: center;
+}
 .black-hawk__content:last-child {
   margin-bottom: 50vh;
-}
+  padding-bottom: 50vh;
+} */
 .black-hawk__content__text-wrapper {
   position: relative;
   background-color: #ffffff;
