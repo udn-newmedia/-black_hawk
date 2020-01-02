892ffa46dc4b1a69146cf3f90a67e31dc18cc235
Jim <wowdacom@gmail.com>
2020-01-02 19:46:54 +0800

    fix background

--- src/components/BlackHawk.vue
+++ src/components/BlackHawk.vue
@@ -7,7 +7,7 @@
         'black-hawk__backgound--active': contentEnterFlag[1],
       }"
       :style="{
-        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_1.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_1.jpg)'
+        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_1_new.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_1_new.jpg)'
       }"
     />
     <div
@@ -16,7 +16,7 @@
         'black-hawk__backgound--active': contentEnterFlag[2],
       }"
       :style="{
-        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_2.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_2.jpg)'
+        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_2_new.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_2_new.jpg)'
       }"
     />
     <div
@@ -25,7 +25,7 @@
         'black-hawk__backgound--active': contentEnterFlag[3],
       }"
       :style="{
-        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_3.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_3.jpg)'
+        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_3_new.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_3_new.jpg)'
       }"
     />
     <div
@@ -34,33 +34,37 @@
         'black-hawk__backgound--active': contentEnterFlag[4],
       }"
       :style="{
-        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_4.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_4.jpg)'
+        backgroundImage: isMob ? 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_4_new.jpg)' : 'url(https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_4_new.jpg)'
       }"
     />
     <div ref="content1" class="black-hawk__content black-hawk__content--first-child">
       <img
         ref="content1_img"
         class="content1_img" 
-        :src="isMob ? 'https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_1.jpg' : 'https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_1.jpg'" alt=""
+        :src="isMob ? 'https://udn.com/newmedia/cms_assets/black_hawk/imgs/mob_1_new.jpg' : 'https://udn.com/newmedia/cms_assets/black_hawk/imgs/Web_1_new.jpg'" alt=""
       >
       <div class="black-hawk__content__text-wrapper">
+        <p class="time">07:55</p>
         <p>機號933、空軍UH-60M黑鷹直升機從松山機場起飛，預計前往宜蘭東澳營區執行春節慰勉行程。</p>
       </div>
     </div>
     <div ref="content2" class="black-hawk__content">
       <div class="black-hawk__content__text-wrapper">
+        <p class="time">08:07</p>
         <p>飛行員失事前最後一次通聯。當時回報能見度7哩，雲高3000呎至5000呎，符合進場標準。</p>
-        <p>失事飛鷹光點在距中正港328.7度17浬處消失。</p>
+        <p>失事黑鷹光點在距中正港328.7度17浬處消失。</p>
       </div>
     </div>
     <div ref="content3" class="black-hawk__content">
       <div class="black-hawk__content__text-wrapper">
+        <p class="time">10:30-14:00</p>
         <p>10:30 軍聞社記者陳映竹用手機回報，被飛機殘骸壓住。</p>
         <p>宜蘭搜救隊、新北搜救隊分別集結出動，朝桶後溪光點消失處搜救。</p>
       </div>
     </div>
     <div ref="content4" class="black-hawk__content black-hawk__content--last-child">
       <div class="black-hawk__content__text-wrapper">
+        <p class="time">14:01</p>
         <p>殘骸發現地。搜救隊回報5人生還、參謀總長沈一鳴等8人不幸罹難。</p>
       </div>
     </div>
@@ -130,8 +134,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.time {
+  font-weight: bolder;
+}
 .black-hawk {
-  background-color: #dedede;
+  /* background-color: #dedede; */
 }
 .black-hawk__backgound {
   pointer-events: none;
@@ -140,9 +147,15 @@ export default {
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-size: cover;
+  background-size: contain;
   opacity: 0;
   transition: .5s ease-in-out;
+  background-repeat: no-repeat;
+}
+@media screen and (min-width: 1440px) {
+  .black-hawk__backgound {
+    background-size: cover;
+  }
 }
 .black-hawk__backgound--active {
   opacity: 1;
@@ -172,13 +185,14 @@ export default {
 .black-hawk__content__text-wrapper {
   position: relative;
   background-color: #ffffff;
-  padding: 40px 0;
+  padding: 20px;
   border-radius: 5px;
 }
 .content1_img {
   position: absolute;
   top: 0;
-  left: 0;
-  width: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100vw;
 }
 </style>
